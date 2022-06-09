cc289613f0b1e55994419bc0fee6ba57700edf62
Minh Nguyen <mnguyen4725@gmail.com>
2022-06-09 16:05:30 -0700

    set up fileReader class and read token from text file

--- src/Discord_bot/BotStartup.java
+++ src/Discord_bot/BotStartup.java
@@ -11,8 +11,12 @@ import javax.security.auth.login.LoginException;
 
 public class BotStartup {
     public static void main(String[] args) throws LoginException {
-        JDABuilder jda = JDABuilder.createDefault("OTI5MjIyMTIxMjQ1Mjc4MjQ4.YdkLiQ.h41Tx6S7yqLltTNVAutuxtfNFms");
-        jda.setActivity(Activity.watching("Your mom"));
+        //get and set token for bot
+        MyFileReader readToken = new MyFileReader("src/Keys/Keys.txt");
+        JDABuilder jda = JDABuilder.createDefault(readToken.getText());
+        readToken.closeFile();
+
+        jda.setActivity(Activity.watching("Your mother"));
         jda.setStatus(OnlineStatus.ONLINE); //can set it to DO NOTDISTURB, IDLE, OFFLINE, INVISIBLE. Setting to invisible or offline is a bad idea
         jda.addEventListeners(new Commands("!"));
         jda.setChunkingFilter(ChunkingFilter.ALL); //allow you to see all the members in the discord server
